4e8f16db0cb3fd418a1bc07f30ab52c6ff98ebb6
Scott Ellis <scottellis94@gmail.com>
2025-02-09 20:02:13 -0500

    revert file validator

--- src/audio/AudioMaster.java
+++ src/audio/AudioMaster.java
@@ -1,24 +1,5 @@
 package audio;
 
-import engine.math.Vec3;
-import engine.utils.TwoTypes;
-import main.Features;
-import main.Main;
-import org.lwjgl.BufferUtils;
-import org.lwjgl.openal.AL;
-import org.lwjgl.openal.AL11;
-import org.lwjgl.openal.ALC;
-import org.lwjgl.openal.ALC10;
-import org.lwjgl.openal.ALC11;
-import org.lwjgl.openal.ALCCapabilities;
-import org.lwjgl.stb.STBVorbis;
-import org.lwjgl.system.MemoryStack;
-import org.lwjgl.system.libc.LibCStdlib;
-import player_local.car.Car;
-import player_local.upgrades.TileNames;
-import player_local.upgrades.Upgrade;
-import settings_and_logging.RSet;
-
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -35,6 +16,26 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 import java.util.Stack;
+
+import org.lwjgl.BufferUtils;
+import org.lwjgl.openal.AL;
+import org.lwjgl.openal.AL11;
+import org.lwjgl.openal.ALC;
+import org.lwjgl.openal.ALC10;
+import org.lwjgl.openal.ALC11;
+import org.lwjgl.openal.ALCCapabilities;
+import org.lwjgl.stb.STBVorbis;
+import org.lwjgl.system.MemoryStack;
+import org.lwjgl.system.libc.LibCStdlib;
+
+import engine.math.Vec3;
+import engine.utils.TwoTypes;
+import main.Features;
+import main.Main;
+import player_local.car.Car;
+import player_local.upgrades.TileNames;
+import player_local.upgrades.Upgrade;
+import settings_and_logging.RSet;
 
 /**
  * 
@@ -98,9 +99,6 @@ public class AudioMaster implements AudioRemote {
 		this.taunts = new HashMap<>();
 
 		this.music = new Source();
-
-		validateDirectoryPath("audio");
-		validateDirectoryPath("audio/music");
 
 		{
 			File musicFolder = new File("audio/music");
@@ -535,13 +533,6 @@ public class AudioMaster implements AudioRemote {
 			this.music.deleteBuffer();
 			var newMusic = loadSoundBuffer(musicLoaded.get(nextSong), buffers, musicBuffer);
 			this.music.play(newMusic.first());
-		}
-	}
-
-	private void validateDirectoryPath(String path) {
-		File file = new File(path);
-		if (!file.isDirectory() || !file.exists()) {
-			throw new RuntimeException("The path [" + path + "] is not a directory or does not exist.");
 		}
 	}
 
--- src/engine/io/Window.java
+++ src/engine/io/Window.java
@@ -46,6 +46,7 @@ import java.nio.BufferUnderflowException;
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
 
+import main.Features;
 import org.lwjgl.BufferUtils;
 import org.lwjgl.PointerBuffer;
 import org.lwjgl.glfw.GLFW;
@@ -69,7 +70,6 @@ import main.Main;
 import main.Texts;
 import scenes.SceneHandler;
 import settings_and_logging.RSet;
-import utils.FileValidator;
 
 public class Window {
 
@@ -169,8 +169,6 @@ public class Window {
 		GLFW.glfwSetWindowFocusCallback(window, (window, focused) -> {
 			this.focused = focused;
 		});
-
-		FileValidator.throwErrorOnMissingDir("./images");
 
 		// ICON
 		setIcon("./images/icon.png");
